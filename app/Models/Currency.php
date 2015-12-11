5850e26ec25e9386722c9db321a5b78f169e4d70
Alexandr Spesivtsev <dart_sas@mail.ru>
2015-12-11 15:40:28 +0700

    init

---
+++ app/Models/Currency.php
@@ -0,0 +1,14 @@
+<?php
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * User: sasik
+ * Date: 12/11/15
+ * Time: 3:29 PM
+ */
+class Currency extends Model
+{
+    public $timestamps = false;
+}
---
+++ database/migrations/2015_12_11_075412_create_currency_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCurrencyTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('currencies', function (Blueprint $table) {
+            $table->increments('id');
+            $table->float('usd');
+            $table->date('date');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('currencies');
+    }
+}
